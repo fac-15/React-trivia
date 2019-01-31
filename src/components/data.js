d820c62f0bac7c022c43266a0c68afa4b705a46b
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-01-31 17:43:39 +0000

    Add functionality to create buttons based on API call

--- src/components/data.js
+++ src/components/data.js
@@ -1,41 +1,40 @@
-import React from "react"
-import { render } from "react-dom"
-import { getTriviaData } from "../utils/getData.js"
+import React from "react";
+import { render } from "react-dom";
+import { getTriviaData } from "../utils/getData.js";
 
 export default class Index extends React.Component {
-    state = {
-      questionData: {},
-      answerData:[]
-    };
+  state = {
+    questionData: {},
+    answerData: []
+  };
 
-    componentDidMount (){
-        getTriviaData()
-         .then(data => {
+  componentDidMount() {
+    getTriviaData().then(data => {
+      const answers = data.map(ele => {
+        //pushes an answer to an array in answerData
+        this.state.answerData.push(ele.answer);
+        //returns an array of objects containing questions and answers
+        console.log(this.state.answerData);
+        return { question: ele.question, answer: ele.answer };
+      });
+      console.log(answers);
 
-            const answers = data.map(
-                (ele) => {
-                    //pushes an answer to an array in answerData
-                    this.state.answerData.push(ele.answer)
-                    //returns an array of objects containing questions and answers
-                    console.log(this.state.answerData)
-                    return  { question: ele.question,
-                              answer: ele.answer
-                }
-            }
-            )
-console.log(answers)
-
-             this.setState({questionData: data[0]})
-             console.log(this.state.questionData)
-            })
-        
-        };
-  
-  
-  
-
-  render(){
-      
-      return <p>{this.state.questionData.question}</p>
+      this.setState({ questionData: data[0] });
+      console.log(this.state.questionData);
+    });
   }
-    }
+
+  render() {
+    let answers = this.state.answerData;
+    return (
+      <div>
+        <p>{this.state.questionData.question}</p>
+        <ul>
+          {answers.map(answer => {
+            return <button>{answer}</button>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
